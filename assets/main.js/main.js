b3c5954226c14df155ad5113d8acc2add0cba6f3
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-25 16:38:20 +0100

    commit di fine esercizio con font cambiati e chiamata api completata

--- assets/main.js/main.js
+++ assets/main.js/main.js
@@ -1,15 +1,30 @@
 const endpoint ="https://lanciweb.github.io/demo/api/pictures/"
+const rowEl = document.querySelector(".row");
+
 
 fetch(endpoint)
     .then(response => response.json())
-    .then(data => {
+    .then(photos => {
         //codice per far qualcosa con la risposta
-        console.log(data);
-    })
+        console.log(photos);
+        photos.forEach(photo => {
+            const { title, url, date } = photo;
+            console.log(title, url, date);
+            rowEl.innerHTML += `
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <img class="pin" src="/img/pin.svg" alt="un semplice pin">
+                    <img src="${url}" class="p-2 " alt="${title}">
+                    <div class="card-body">
+                        <p class="card-text">
+                        ${date}<br>
+                        <strong>${title}</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>`;
+            })
+        })
+        
     .catch(error => {
-        //codice per far qualcosa in caso di errore
-        console.log(error);
-    })
-
-
-
+        console.error(error)})
---
+++ readme.md
@@ -0,0 +1,12 @@
+<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <img class="pin" src="/img/pin.svg" alt="un semplice pin">
+                            <img  class="p-2" src="/img/cork.png" class="card-img-top" alt="cork">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Accusamus Beatae Ad Facilis 
+                                    Cum Similique Qui Sunt
+                                </p>
+                            </div>
+                        </div>
+                    </div>   
